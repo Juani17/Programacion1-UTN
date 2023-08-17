d4b890f32f3134903def98d99c2e34ca017b11db
Juani17 <128089659+Juani17@users.noreply.github.com>
2023-08-17 09:27:29 -0300

    Add files via upload

---
+++ Complementarios frery.py
@@ -0,0 +1,44 @@
+#Ejercicios complementarios Juani Frery
+
+numero1= 15   #1- definimos una variable llamada numero1 y le asignamos 15 de valor
+numero2= 14.9 #2- definimos otra variable llamada numero2 y le asignamos 14.9 de valor
+
+suma= (numero1 + numero2) #3- Creamos una variable llamada suma, la cual le asignamos el valor de numero1 sumado a numero2
+
+resta= (numero1 - numero2) #4- Creamos una variable llamada resta, la cual le asignamos el valor de numero1 restado a numero2
+
+multiplicacion= (numero1 * numero2) #4- Creamos una variable llamada multiplicacion, la cual le asignamos el valor de numero1 multiplicado a numero2
+
+division= (numero1 / numero2) #4- Creamos una variable llamada division, la cual le asignamos el valor de numero1 dividido a numero2
+
+print("La suma de ", numero1, " y ", numero2,  "es: ", suma, "La resta de ", numero1, " y ", numero2,  "es: ", resta, "La multiplicación de ", numero1, " y ", numero2,  "es: ", multiplicacion, "La división de ", numero1, " y ", numero2,  "es: ", division) #4- imprimimos las variables
+
+nombre = "Juani Frery" #5- Creamos un a variable llamada nombre y le asigno mi nombre
+
+precio= 500.50 #6- Creamos un a variable llamada precio y le asigno 500.50 de valor
+
+descuento= 0.43 #7- Creamos un a variable llamada descuento y le asigno el valor de 43%
+
+precio_final = (precio * descuento) #8- Creamos un a variable llamada precio_final y le asigno el valor de la variable precio menos el 43%
+
+cadena = "Estoy estudiando python" #9- Creamos un avariable llamada cadena y le agregamos una frase
+
+longitud = len(cadena) #10- Creamos una variable llamada longitud y le asignamos el valor de la cantidad de caracteres de la variable llamada cadena
+
+precio = int(18.8)  #11-  Creamos nuevamente la variable precio y le asignamos un valor decimal, y a su vez lo transformamos a entero
+
+nombre = "Juani" #12 Creamos una variable llamada nombre y le asigno "Juani"
+
+apellio= "Frery" #12 Creamos una variable llamada apellido y le asigno "Frery"
+
+nombre_completo = (nombre + " " + apellio) #12 Creamos una variable llamada nombre_completo y le asigno la concatenacion de las variables nombre y apellido
+
+edad = (18 + 5 -10) #13 Creamos una variable llamada edad y le asigno mi edad 18 sumado en 5 y le resto 10
+
+altura = ((1.70 * 4 )/ 3) #14 Creamos una variable llamada altura y le asigno mi altura en decimales 1.70 multipliicada por 4 y dividida por 3
+
+nombrem = "JUANI" #15 Creamos una variable donde le asigno mi nombre en mayùsculas
+
+nombrem.lower() #15 Transformamos el contenido de la variable nombrem a minusculas
+
+print(nombrem.title()) #16 Imprimimos la variable nombrem con la primer letra en mayuscula y las demas en minusculas
